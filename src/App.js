b9d35356f091b3c5f5628f1577ed5b40d6d3d624
Sarah Ng <93563644+sarah-ernee@users.noreply.github.com>
2024-09-07 16:37:29 +0800

    added background image and dark mode toggling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import Button from "react-bootstrap/Button";
-// import Card from "react-bootstrap/Card";
 import QuoteBubble from "./components/QuoteBubble";
+import "./App.css";
+
+import { FaRegMoon, FaRegSun } from "react-icons/fa";
 
 export default function App() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
 
   async function updateQuote() {
+    setIsLoading(true);
+
     try {
       const response = await fetch("https://api.quotable.io/random");
 
@@ -18,28 +23,37 @@ export default function App() {
       setData(data);
     } catch (err) {
       console.error(err);
-      setData({ content: "Oops...something went wrong" });
+    } finally {
+      setIsLoading(false);
     }
   }
-
-  // weird spacing between apostrophe and s - probably a html inject issue
-  // bubble should grow with text received
-  // control author name length
-  // adjust tilt of author bubble
 
   useEffect(() => {
     updateQuote();
   }, []);
 
-  if (!data) return null;
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
-    <div className="App">
-      <QuoteBubble
-        character={data.author}
-        message={data.content}
-        onRefreshQuote={updateQuote}
-      />
+    <div className={`App ${darkMode ? "dark-bg" : "light-bg"}`}>
+      <button
+        className="toggle-btn"
+        onClick={toggleDarkMode}
+        style={{ alignSelf: "end" }}
+      >
+        {darkMode ? <FaRegSun /> : <FaRegMoon />}
+      </button>
+      <div className="content-row">
+        <QuoteBubble
+          character={isLoading ? "Loading..." : data?.author}
+          message={isLoading ? "Loading..." : data?.content}
+          onRefreshQuote={updateQuote}
+          isLoading={isLoading}
+          darkMode={darkMode}
+        />
+      </div>
     </div>
   );
 }
--- src/components/QuoteBubble.js
+++ src/components/QuoteBubble.js
@@ -1,24 +1,50 @@
 import React from "react";
 import "./QuoteBubble.css";
 
-const QuoteBubble = ({
-  character = "Tom Nook",
-  message = "I am once again asking you to pay your mortgage, dear camper",
-  onRefreshQuote
-}) => {
+const QuoteBubble = ({ character, message, onRefreshQuote, darkMode }) => {
+  const calculateFontSize = (messageLength) => {
+    const minFontSize = 18;
+    const maxFontSize = 32;
+    const lengthThreshold = 100;
+
+    // Calculate the new font size
+    const fontSize = Math.max(
+      minFontSize,
+      maxFontSize - Math.max(0, messageLength - lengthThreshold) / 5
+    );
+
+    return `${fontSize}px`;
+  };
+
+  const chars = message.length ? message.length : 0;
+  const fontSize = calculateFontSize(chars);
+
   return (
     <div className="dialogue">
       <div className="dialogue-blobs">
-        <div className="dialogue-blob-top"></div>
-        <div className="dialogue-blob-bottom"></div>
-        <div className="dialogue-text">{message}</div>
+        <div className={`${darkMode ? "top-blob-dark" : "top-blob"}`}></div>
+        <div
+          className={`${darkMode ? "bottom-blob-dark" : "bottom-blob"}`}
+        ></div>
+        <div
+          className={`${darkMode ? "dialogue-text-dark" : "dialogue-text"}`}
+          style={{ fontSize }}
+        >
+          {message}
+        </div>
       </div>
 
       <div className="dialogue-character-wrap">
-        <div className="dialogue-character">{character}</div>
+        <div
+          className={`${
+            darkMode ? "dialogue-character-dark" : "dialogue-character"
+          }`}
+        >
+          {character}
+        </div>
       </div>
 
-      <svg
+      {/* <svg
         className="arrow"
         width="45"
         height="25"
@@ -31,7 +57,10 @@ const QuoteBubble = ({
           d="M22.5 25C18.0184 25 7.59473 12.6404 1.55317 4.96431C-0.122281 2.83559 1.72264 -0.179893 4.39835 0.243337C10.2831 1.17415 18.2164 2.28736 22.5 2.28736C26.7836 2.28736 34.7169 1.17415 40.6017 0.243339C43.2774 -0.17989 45.1223 2.83559 43.4468 4.96431C37.4053 12.6404 26.9816 25 22.5 25Z"
           fill="#F1AE04"
         />
-      </svg>
+      </svg> */}
+      <button className="quote-btn" onClick={onRefreshQuote}>
+        New Quote
+      </button>
     </div>
   );
 };
